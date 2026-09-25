366b1d2776740666e6740c00f761f9698e86029f
auxz2jz <41704988+auxz2jz@users.noreply.github.com>
2026-09-25 02:24:33 -0700

    Add AMF and X3D to supported model formats

--- app/src/main/java/com/edgar/viewer3d/ModelImporter.kt
+++ app/src/main/java/com/edgar/viewer3d/ModelImporter.kt
@@ -1,7 +1,7 @@
 package com.edgar.viewer3d
 
 object ModelImporter {
-    private val supported = setOf("glb", "gltf", "stl", "obj", "ply", "off", "3mf")
+    private val supported = setOf("glb", "gltf", "stl", "obj", "ply", "off", "3mf", "amf", "x3d")
 
     fun extension(fileName: String) = fileName.substringAfterLast('.', "").lowercase()
     fun isSupported(fileName: String) = extension(fileName) in supported
@@ -27,7 +27,9 @@ object ModelImporter {
             "ply" -> fromMesh("PLY", bytes, PlyParser.parse(fileName, bytes))
             "off" -> fromMesh("OFF", bytes, OffParser.parse(fileName, bytes))
             "3mf" -> fromMesh("3MF", bytes, ThreeMfParser.parse(fileName, bytes))
-            else -> error("Unsupported file type .$ext. Supported: GLB, glTF, STL, OBJ, PLY, OFF, 3MF.")
+            "amf" -> fromMesh("AMF", bytes, AmfParser.parse(fileName, bytes))
+            "x3d" -> fromMesh("X3D", bytes, X3dParser.parse(fileName, bytes))
+            else -> error("Unsupported file type .$ext. Supported: GLB, glTF, STL, OBJ, PLY, OFF, 3MF, AMF, X3D.")
         }
     }
 
