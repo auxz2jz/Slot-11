60ccad1fba801efa10bda65a96929a4f34d5c3d4
auxz2jz <41704988+auxz2jz@users.noreply.github.com>
2026-09-24 23:53:10 -0700

    Bump Android 3D Viewer to v0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.edgar.viewer3d"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
