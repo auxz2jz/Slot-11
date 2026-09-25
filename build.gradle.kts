c52423701c707f7673365cdd3760ce22ebc5daa2
auxz2jz <41704988+auxz2jz@users.noreply.github.com>
2026-09-24 23:11:58 -0700

    Use Kotlin 2.4.20 for Filament 1.77 metadata compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
+    }
+}
+
 plugins {
     id("com.android.application") version "9.4.1" apply false
 }
