f30e5ee30583d06b60b6f9002ac39ee136d4aa1e
auxz2jz <41704988+auxz2jz@users.noreply.github.com>
2026-09-24 23:04:12 -0700

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.edgar.viewer3d"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.edgar.viewer3d"
+        minSdk = 26
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        resources.excludes += setOf(
+            "META-INF/DEPENDENCIES",
+            "META-INF/LICENSE",
+            "META-INF/LICENSE.txt",
+            "META-INF/NOTICE",
+            "META-INF/NOTICE.txt"
+        )
+    }
+}
+
+dependencies {
+    val filamentVersion = "1.77.1"
+    implementation("com.google.android.filament:filament-android:$filamentVersion")
+    implementation("com.google.android.filament:gltfio-android:$filamentVersion")
+    implementation("com.google.android.filament:filament-utils-android:$filamentVersion")
+}
